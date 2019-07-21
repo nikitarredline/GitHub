9bfecd6512a0f01ec1537cbde2baca9c1832a2e4
nikitarredline <glad4@mail.ru>
2019-07-21 14:19:40 +0300

    1

--- test_items.py
+++ test_items.py
@@ -1,8 +1,8 @@
 import time
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_items(browser):
     browser.get(link)
-    time.sleep(10)
+    time.sleep(5)
     browser.find_element_by_xpath("//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
