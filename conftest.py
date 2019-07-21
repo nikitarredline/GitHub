bfef627ae7652a8532391c0216e3dd0b11b49889
nikitarredline <glad4@mail.ru>
2019-07-21 14:23:12 +0300

    1

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+                     help="lang")
 
 @pytest.fixture(scope="function")
 def browser(request):
